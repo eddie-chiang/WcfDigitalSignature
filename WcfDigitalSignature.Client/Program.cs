90b7e4aa86cdf2cac708ce6ba899aaae944ac41b
Eddie Chiang <echiang@fraedom.com>
2017-07-20 10:28:25 +1200

    Add project files.

---
+++ WcfDigitalSignature.Client/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfDigitalSignature.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
